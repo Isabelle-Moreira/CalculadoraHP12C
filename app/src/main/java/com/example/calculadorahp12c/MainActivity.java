ed562b87fb0281b3a19b561d313d3c82c3d7101a
isabelle Moreira <isabelleyasmor@gmail.com>
2024-07-31 10:32:45 -0300

    Inicio interface

--- app/src/main/java/com/example/calculadorahp12c/MainActivity.java
+++ app/src/main/java/com/example/calculadorahp12c/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.calculadorahp12c;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button botaoUm;
+    private Button botaoDois;
+    private Button botaoTres;
+    private Button botaoQuatro;
+    private Button botaoCinco;
+    private Button botaoSeis;
+    private Button botaoSete;
+    private Button botaoOito;
+    private Button botaoNove;
+    private Button botaoPonto;
+    private Button Clear;
+    private Button Enter;
+    calculadora calculadora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        botaoUm=findViewById(R.id.um);
+        botaoDois=findViewById(R.id.dois);
     }
 }
